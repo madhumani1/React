d4626ecbe8a3a01424d318752eb5612ba4e11d0d
Madhukar Mani <madhumani1@gmail.com>
2020-10-15 22:25:05 -0400

    added comments and reconciliation in action

--- react-redux-doctors/src/App.js
+++ react-redux-doctors/src/App.js
@@ -1,29 +1,50 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Text from './components/Text';
+import { render } from '@testing-library/react';
+
 
 function App() {
-  const [count, setcount] = useState(() => {
-    console.log('run function')
-    return 0;
-  })
+  /**
+   * The argument to useState is the initial value for the state element
+   * const [state,functionToSetStateValue] = useState(initialStateValue);
+   * To support dynamic elements, JSX uses {}. 
+   * For return(<button>{expression}</button>); 
+   * will give you a value based on resolution of expression.
+   * To make stateful object, we need to make a state object.
+   * To make state object, we need to use a hook called useState
+   * 
+   * syntax of useState 
+   * useState returns 2 objects and 1 argument - 
+   * state object (getter) and updater function (setter) to change that object.
+   * Object can be of any type you wish it to be. 
+   * String, Number, array or even another component.
+   */
+  const [count,setCount] = useState(() => {
+      console.log('run function');
+      return 0;
+    }
+  );
 
-  function decrementCount() {
-    setcount(prevCount => prevCount-1)
-  }
+  const onIncrement = (hopper) => setCount(count+hopper);
+  const onDecrement = (hopper) => setCount(count-hopper);
+  // if you prefer ONLY non negative numbers
+  //(count<=0)?setCount(count):setCount(count-hopper);
 
-  function incrementCount() {
-    setcount(prevCount => prevCount+1)
-  }
-
-  return (
+  return(
     <>
-    <button onClick={decrementCount}>-</button>
-  <span>{count}</span>
-    <button onClick={incrementCount}>+</button>
+      <Button onButtonClick={onDecrement} name='-' hopper={10}/>
+      <Button onButtonClick={onDecrement} name='-' hopper={5}/>
+      <Button onButtonClick={onDecrement} name='-' hopper={1}/>&nbsp;&nbsp;&nbsp;&nbsp;
+      <Text value={count} />&nbsp;&nbsp;&nbsp;&nbsp;
+      <Button onButtonClick={onIncrement} name='+' hopper={1}/>
+      <Button onButtonClick={onIncrement} name='+' hopper={5}/>
+      <Button onButtonClick={onIncrement} name='+' hopper={10}/>
     </>
   );
-  //return (
+  // return(
   //   <div className="App">
   //     <header className="App-header">
   //       <img src={logo} className="App-logo" alt="logo" />
@@ -41,7 +62,6 @@ function App() {
   //     </header>
   //   </div>
   // );
-
 }
 
 export default App;
--- react-redux-doctors/src/index.js
+++ react-redux-doctors/src/index.js
@@ -2,14 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GitApp from './components/GitApp';
+// import Button from './components/Button';
+// import Display from './components/Display';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <GitApp title='The GitHub Cards App' />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Learn Reconciliation in action
+
+// const render = () => {
+//   document.getElementById('mountNode').innerHTML = `
+//   <div>
+//     Hello HTML
+//     <br/>
+//     <input type=text"/>
+//     <br/>
+//     <pre>${(new Date).toLocaleTimeString()}</pre>
+//   </div>
+//   `;
+//   ReactDOM.render(
+//     React.createElement(
+//       "div",null,"Hello React",
+//       React.createElement('br', null), 
+//       React.createElement('input', null),
+//       React.createElement('br', null), 
+//       React.createElement('pre', null, (new Date).toLocaleTimeString()), 
+//     ),
+//     document.getElementById('mountNode2'),
+// ); 
+// }
+
+// document.getElementById('mountNode').innerHTML = `
+//   <div>
+//     Hello HTML
+//     <br/>
+//     <input type=text"/>
+//     <br/>
+//     <pre>${(new Date).toLocaleTimeString()}</pre>
+//   </div>
+// `;
+// ReactDOM.render(
+//   React.createElement(
+//     "div",null,"Hello React",
+//     React.createElement('br', null), 
+//     React.createElement('input', null),
+//     React.createElement('br', null), 
+//     React.createElement('pre', null, (new Date).toLocaleTimeString()), 
+//   ),
+//   document.getElementById('mountNode2'),
+// );
+// setInterval(render, 1000);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
